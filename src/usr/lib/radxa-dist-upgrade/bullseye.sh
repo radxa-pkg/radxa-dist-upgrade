4d61607a3d68f78aa90a2e403ac6309eb3959800
CodeChenL <2540735020@qq.com>
2024-07-08 17:05:48 +0800

    refactor: move some common funciton

--- src/usr/lib/radxa-dist-upgrade/bullseye.sh
+++ src/usr/lib/radxa-dist-upgrade/bullseye.sh
@@ -9,56 +9,6 @@ checks() {
     STEP="1"
 }
 
-check_packages() {
-    local product package
-    product="$(tr $"\0" $"\n" < /proc/device-tree/compatible | tail -n 2 | head -n 1 | cut -d "," -f 2)"
-    package="task-$product"
-
-    if [[ "$(dpkg --get-selections "$package" | awk '{print $2}')" == "install" ]]
-    then
-        echo "$package is installed."
-    else
-        if yesno "$package is not installed. Do you want to install it?"
-        then
-            echo "Installing $package"
-            if apt-get install -y "$package"
-            then
-                echo "$package installed."
-            else
-                echo "Failed to install $package."
-                return 1
-            fi
-        else
-            echo "Skipping $package installation."
-            return 1
-        fi
-    fi
-}
-
-check_system_upgrade() {
-    apt-get update
-    upgradable="$(apt-get -s upgrade)"
-
-    if echo "$upgradable" | grep -q "0 upgraded, 0 newly installed, 0 to remove and 0 not upgraded"; then
-        echo "All packages are up to date."
-    else
-        if yesno "Some packages are not up to date, please upgrade first"
-        then
-            system_upgrade
-        fi
-    fi
-}
-
-check_dkms_status() {
-    if [[ "$(dkms status | awk '{print $5}' | grep -v installed)" == "" ]]
-    then
-        echo "DKMS is ready."
-    else
-        echo "Some dkms modules are not installed, please check "dkms status""
-        return 1
-    fi
-}
-
 get_source_list() {
     for file in /etc/apt/sources.list /etc/apt/sources.list.d/*.list
     do
